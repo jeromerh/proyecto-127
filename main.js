1935c239d0af31623e3460346c819fbda4753002
jeromerh <119083415+jeromerh@users.noreply.github.com>
2023-12-12 16:49:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+sound=""
+function setup(){
+canvas.createCanvas(600,500)
+canvas.center()
+video=createCapture(VIDEO)
+video.hide()
+}
+function preload(){
+sound=loadSound("music.mp3")
+}
+function draw(){
+ image(video,0,0,600,500)   
+}
